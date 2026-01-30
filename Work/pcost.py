6006931ab51311119a6bb2bb8fd18528384c73a7
hammer-cjj <512823985@qq.com>
2026-01-30 18:38:27 +0800

    Completed ex1_4.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,15 @@
-with open("../Data/portfolio.dat", "r") as f:
-    sum = 0.0
-    for line in f:
-        item = line.split()
-        sum += int(item[1]) * float(item[2])
-print("sum =", sum)
+def portfolio_cost(filename):
+    """Compute the total cost of the portfolio."""
+    with open(filename, "r") as f:
+        sum = 0.0
+        for line in f:
+            try:
+                item = line.split()
+                sum += int(item[1]) * float(item[2])
+            except ValueError as e:
+                print("Couldn't parse:", line[:-1])
+                print("Reason:", e)
+    return sum
+
+
+print(portfolio_cost("../Data/portfolio3.dat"))
